98a38251bb9deb769efb7d3f719ac2c34b0e5722
Phil Winder <phil@WinderResearch.com>
2021-04-13 16:40:21 +0100

    refactor: move data to more obvious directory

--- src/download.py
+++ src/download.py
@@ -13,7 +13,7 @@ def extract_tar_from_url(data_dir: components.OutputPath(str)):
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
-    url = "https://raw.githubusercontent.com/SAME-Project/example-kubeflow-CrossSell/main/code/data/external/data.tar.gz"
+    url = "https://raw.githubusercontent.com/SAME-Project/example-kubeflow-CrossSell/main/data/external/data.tar.gz"
     logger.info(f"Downloading {url}")
     stream = urllib.request.urlopen(url)
     tar = tarfile.open(fileobj=stream, mode="r|gz")
